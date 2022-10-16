23660de0c91dccd28be9e1a41eeb1bdd2a446b75
mark46wei <ohbom292@gmail.com>
2022-10-16 16:31:14 +0800

    app

--- app.py
+++ app.py
@@ -1,41 +1,48 @@
-from __future__ import unicode_literals
-import os
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
+#載入LineBot所需要的模組 
+from flask import Flask, request, abort  
+from linebot import (
+    LineBotApi, WebhookHandler
+ ) 
+from linebot.exceptions import (
+    InvalidSignatureError
+ ) 
+from linebot.models import *
 
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+app = Flask(__name__)  
+# 必須放上自己的Channel Access Token 
+line_bot_api = LineBotApi('mvwVq2sJsehGFk4S+EcZZIE+iiiz84X75F25OmzbBAWmN38CSS9b0peFsnyo/pVZmelWV7FKo+0idSInWPFIhB/FKEurySO72fRnW4qifQhS1B1JvoJSBGSFpDqqqKBcsSYCPDRV5ToZUslzdGg4eAdB04t89/1O/w1cDnyilFU=')  
+# 必須放上自己的Channel Secret
+handler = WebhookHandler('db6a92064c543b1380b52c8eab14d646')  
 
-app = Flask(__name__)
+line_bot_api.push_message('Uba61a3d45b7acb0b333ece913cebd893', TextSendMessage(text='你可以開始了'))
 
-# LINE 聊天機器人的基本資料
-line_bot_api = LineBotApi('mvwVq2sJsehGFk4S+EcZZIE+iiiz84X75F25OmzbBAWmN38CSS9b0peFsnyo/pVZmelWV7FKo+0idSInWPFIhB/FKEurySO72fRnW4qifQhS1B1JvoJSBGSFpDqqqKBcsSYCPDRV5ToZUslzdGg4eAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('db6a92064c543b1380b52c8eab14d646')
-
-# 接收 LINE 的資訊
-@app.route("/callback", methods=['POST'])
-def callback():
+#line bot與heroku串接
+# 監聽所有來自 /callback 的 Post Request 
+@app.route("/callback", methods=['POST']) 
+def callback():     
+    # get X-Line-Signature header value     
     signature = request.headers['X-Line-Signature']
-
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
+    # get request body as text     
+    body = request.get_data(as_text=True)     
+    app.logger.info("Request body: " + body)      
+    # handle webhook body     
+    try:         
+        handler.handle(body, signature)     
+    except InvalidSignatureError:         
+        abort(400)      
     return 'OK'
 
-# 學你說話
-@handler.add(MessageEvent, message=TextMessage)
-def echo(event):
-    
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
+#訊息傳遞區塊 
+##### 基本上程式編輯都在這個function ##### 
+@handler.add(MessageEvent, message=TextMessage) 
+def handle_message(event):     
+    message = TextSendMessage(text=event.message.text)     
+    line_bot_api.reply_message(event.reply_token,message)
 
-if __name__ == "__main__":
-    app.run()
+#主程式 
+import os 
+if __name__ == "__main__":    
+    port = int(os.environ.get('PORT', 5000))     
+    app.run(host='0.0.0.0', port=port)
+
+#Wei測試串    
